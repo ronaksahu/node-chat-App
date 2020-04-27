98ee082be7f58c02c49d8265af1a3fe87044eedf
ronaksahu0204 <ronaksahu0204@gmail.com>
2020-04-27 22:32:24 +0530

    'changedddd'

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.emit('message', generateMessage('Admin', 'Welcome!!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has Joined the chat`))
         io.to(user.room).emit('roomData', {
             room: user.room,
